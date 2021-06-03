3f8aee5f82d6052ca817b6e5fc227346f04d5a38
Ihor Karpyn <igor82200@gmail.com>
2021-06-03 13:12:14 +0300

    Updates

---
+++ precache-manifest.efd1385651a048daa14e1ce3bbb05668.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3d0ae700efa1b03c032b391c0d88abc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "67a033b4a3af35aad73b",
+    "url": "./static/css/2.f6a27176.chunk.css"
+  },
+  {
+    "revision": "42f0354ebd30227b4e67",
+    "url": "./static/css/main.9aaf0c5f.chunk.css"
+  },
+  {
+    "revision": "67a033b4a3af35aad73b",
+    "url": "./static/js/2.4b598178.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "./static/js/2.4b598178.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42f0354ebd30227b4e67",
+    "url": "./static/js/main.e666234c.chunk.js"
+  },
+  {
+    "revision": "aae0386031766cdb5520",
+    "url": "./static/js/runtime-main.63136dd4.js"
+  }
+]);
